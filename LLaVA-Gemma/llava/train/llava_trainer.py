c22773955098ae6ef45cfce9c3c8973ba890f175
Zhang, Yanli L <yanli.l.zhang@intel.com>
2024-09-03 01:42:10 -0700

    HS-3014 - Save result to HF convertable ckpt
    Signed-off-by: Zhang, Yanli L <yanli.l.zhang@intel.com>

--- LLaVA-Gemma/llava/train/llava_trainer.py
+++ LLaVA-Gemma/llava/train/llava_trainer.py
@@ -276,7 +276,4 @@ class LLaVATrainer(Trainer):
         super(LLaVATrainer, self)._save_checkpoint(model, trial, metrics)
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
-        if getattr(self.args, 'tune_mm_mlp_adapter', False):
-            pass
-        else:
-            super(LLaVATrainer, self)._save(output_dir, state_dict)
+        super(LLaVATrainer, self)._save(output_dir, state_dict)
